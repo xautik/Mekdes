148f500efb01440b3ce4113d2774ba0b9f93a610
Kayodee <avocarterp@gmail.com>
2024-07-05 12:27:20 +0300

    Triangle

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+    $("body").addClass("loaded");
+  };
+
 let button = document.getElementById("top");
 window.onscroll = function() {scrollFunction()};
 
@@ -18,3 +22,45 @@ function clearInp(){
     document.getElementsByTagName("input").value=""
     /*document.getElementsByTagName("input").reset()*/
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+    const images = document.querySelectorAll(".gallery-body img")
+    const modal = document.querySelector(".modal")
+    const modalImg = document.querySelector(".modal-img")
+    const closebtn = document.querySelector(".close")
+    let currentIndex = 0
+
+    const prevBtn = document.querySelector(".btnPrev")
+    const nextBtn = document.querySelector(".btnNext")
+
+    images.forEach((image, index) => {
+        image.addEventListener("click", function(){
+            currentIndex = index
+            updateModalContent()
+            modal.classList.add("show")
+        })
+    })
+
+    function updateModalContent(){
+        const image = images[currentIndex]
+        modalImg.classList.remove("show")
+        setTimeout(() => {
+            modalImg.src = image.src
+            modalImg.classList.add("show")
+        }, 300);
+    }
+
+    nextBtn.addEventListener("click", function(){
+        currentIndex = currentIndex + 1 >= images.length ? 0 : currentIndex + 1
+        updateModalContent()
+    })
+
+    prevBtn.addEventListener("click", function(){
+        currentIndex = currentIndex - 1 < 0 ? images.length - 1 : currentIndex - 1
+        updateModalContent()
+    })
+
+    closebtn.addEventListener("click", function(){
+        modal.classList.remove("show")
+    })
+})
